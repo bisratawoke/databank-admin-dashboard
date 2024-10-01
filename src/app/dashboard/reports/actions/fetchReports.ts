51c0aece449b034c920983a2cce20b5e95bc54cf
bisratawoke <bisratawoke52@gmail.com>
2024-10-01 15:34:11 +0300

    wip

--- src/app/dashboard/reports/actions/fetchReports.ts
+++ src/app/dashboard/reports/actions/fetchReports.ts
@@ -1,31 +1,29 @@
-"use server"
+"use server";
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_URL = process.env.BACKEND_URL;
 export async function fetchReports() {
-    try {
-        const res = await fetch(`${API_URL}/reports`, {
-            headers: {
-                "Cache-Control": "no-store", // Prevent caching on the server side
-            },
-            cache: "no-cache", // Ensure no cache is used on the client side
-        });
-        const result = await res.json();
-        return result;
-    } catch (err) {
-        console.log("=========== in action error =======");
-        console.log(err);
-    }
-}
-
-export async function fetchReport(id: string) {
-    const res = await fetch(`${API_URL}/reports/${id}`, {
-        headers: {
-            "Cache-Control": "no-store",
-        },
-        cache: "no-cache",
+  try {
+    const res = await fetch(`${API_URL}/reports`, {
+      headers: {
+        "Cache-Control": "no-store", // Prevent caching on the server side
+      },
+      cache: "no-cache", // Ensure no cache is used on the client side
     });
     const result = await res.json();
     return result;
+  } catch (err) {
+    console.log("=========== in action error =======");
+    console.log(err);
+  }
 }
 
-
+export async function fetchReport(id: string) {
+  const res = await fetch(`${API_URL}/reports/${id}`, {
+    headers: {
+      "Cache-Control": "no-store",
+    },
+    cache: "no-cache",
+  });
+  const result = await res.json();
+  return result;
+}
--- src/app/dashboard/reports/actions/updateReport.ts
+++ src/app/dashboard/reports/actions/updateReport.ts
@@ -2,20 +2,22 @@
 
 import { Data } from "../types";
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const updateReport = async (reportId: string, data: Data[]): Promise<{ result: any; status: number }> => {
-    const response = await fetch(`${API_URL}/reports/${reportId}`, {
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({ data }),
-        method: 'PUT',
-    });
-    const result = await response.json();
+const API_URL = process.env.BACKEND_URL;
+export const updateReport = async (
+  reportId: string,
+  data: Data[]
+): Promise<{ result: any; status: number }> => {
+  const response = await fetch(`${API_URL}/reports/${reportId}`, {
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ data }),
+    method: "PUT",
+  });
+  const result = await response.json();
 
-    return {
-        result,
-        status: response.status,
-    };
+  return {
+    result,
+    status: response.status,
+  };
 };
-
