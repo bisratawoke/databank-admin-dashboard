40d41e9e46b3584e16be5311f7558e037b0c47f4
bisratawoke <bisratawoke52@gmail.com>
2024-10-01 11:21:40 +0300

    moved the add and delete button to a central folder in the root dashboard/components folder

--- src/app/dashboard/reports/actions/createData.ts
+++ src/app/dashboard/reports/actions/createData.ts
@@ -2,23 +2,23 @@
 
 import { Data } from "../types";
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_URL = process.env.BACKEND_URL;
 
+export async function createData(
+  dataEntries: { field: string; value: string }[]
+) {
+  console.log("dataEntiries: ", dataEntries);
+  const res = await fetch(`${API_URL}/data/bulk`, {
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(dataEntries), // Wrap dataEntries in an object
+    method: "POST",
+  });
+  const result = await res.json();
 
-export async function createData(dataEntries: { field: string; value: string }[]) {
-
-    console.log("dataEntiries: ", dataEntries)
-    const res = await fetch(`${API_URL}/data/bulk`, {
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify(dataEntries), // Wrap dataEntries in an object
-        method: "POST",
-    });
-    const result = await res.json();
-
-    return {
-        result,
-        status: res.status,
-    };
+  return {
+    result,
+    status: res.status,
+  };
 }
