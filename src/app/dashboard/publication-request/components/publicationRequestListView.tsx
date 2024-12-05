2ff1055852ab44a24ea13edb85ec4324016596a9
bisratawoke <bisratawoke52@gmail.com>
2024-12-05 10:36:54 +0300

    Implemented routing between publication request list view to publication request detail view page

--- src/app/dashboard/publication-request/components/publicationRequestListView.tsx
+++ src/app/dashboard/publication-request/components/publicationRequestListView.tsx
@@ -1,16 +1,22 @@
 "use client";
 import { Table } from "antd";
+import PublicationStatusTag from "./publicationStatusTag";
+import { useRouter } from "next/navigation";
 export default function PublicationRequestListView({
   publicationRequestList,
 }: {
   publicationRequestList: Array<Record<string, any>>;
 }) {
+  const router = useRouter();
   const columns = [
     { title: "Request Id", dataIndex: "_id", key: "_id" },
     {
       title: "Request Status",
       dataIndex: "status",
       key: "status",
+      render: (value: string) => {
+        return <PublicationStatusTag value={value} />;
+      },
     },
     {
       title: "Purpose for Research",
@@ -24,8 +30,13 @@ export default function PublicationRequestListView({
       columns={columns}
       size="small"
       bordered
-      onRow={(row) => {
-        return {};
+      onRow={(record) => {
+        return {
+          onClick: () => {
+            router.push(`/dashboard/publication-request/${record._id}`);
+          },
+          onMouseEnter: () => {},
+        };
       }}
     />
   );
--- src/app/dashboard/publication-request/components/publicationStatusTag.tsx
+++ src/app/dashboard/publication-request/components/publicationStatusTag.tsx
@@ -2,6 +2,11 @@
 
 import { Tag } from "antd";
 
-export default function PublicationStatusTag(value: string) {
-  return <Tag></Tag>;
+export default function PublicationStatusTag({ value }: { value: string }) {
+  switch (value) {
+    case "Deputy Approved":
+      return <Tag color="green">{value}</Tag>;
+    default:
+      return <Tag color="orange">{value}</Tag>;
+  }
 }
